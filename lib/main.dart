84404bb186f05ee125268fe91427322913c8740e
deevseek <nugraha.deev@gmail.com>
2026-01-16 14:14:13 +0700

    Add auth gate to show login before customers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/auth/presentation/auth_controller.dart';
+import 'features/auth/presentation/login_page.dart';
 import 'features/customers/presentation/customer_list_page.dart';
 
 void main() {
@@ -20,7 +22,50 @@ class MyApp extends StatelessWidget {
           border: OutlineInputBorder(),
         ),
       ),
-      home: const CustomerListPage(),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  final _authController = AuthController();
+
+  @override
+  void initState() {
+    super.initState();
+    _authController.initialize();
+  }
+
+  @override
+  void dispose() {
+    _authController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _authController,
+      builder: (context, _) {
+        switch (_authController.status) {
+          case AuthStatus.initial:
+          case AuthStatus.loading:
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          case AuthStatus.authenticated:
+            return const CustomerListPage();
+          case AuthStatus.unauthenticated:
+            return LoginPage(controller: _authController);
+        }
+      },
     );
   }
 }
